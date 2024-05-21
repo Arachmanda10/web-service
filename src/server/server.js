4b50a8980fe1d47b024a9228b155e7f500596702
Arachmanda10 <93859083+Arachmanda10@users.noreply.github.com>
2024-05-21 10:58:29 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -2,35 +2,37 @@ const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
 const InputError = require('../exceptions/InputError');
- 
+
 (async () => {
+    const port = process.env.PORT || 3000;
+
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
+        port: port,
+        host: '0.0.0.0', // Gunakan 0.0.0.0 agar dapat diakses secara eksternal
         routes: {
             cors: {
-              origin: ['*'],
+                origin: ['*'],
             },
         },
     });
- 
+
     const model = await loadModel();
     server.app.model = model;
- 
+
     server.route(routes);
- 
+
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
- 
+
         if (response instanceof InputError) {
             const newResponse = h.response({
                 status: 'fail',
                 message: `${response.message}`
-            })
-            newResponse.code(response.statusCode)
+            });
+            newResponse.code(response.statusCode);
             return newResponse;
         }
- 
+
         if (response.isBoom) {
             const newResponse = h.response({
                 status: 'fail',
@@ -39,11 +41,10 @@ const InputError = require('../exceptions/InputError');
             newResponse.code(response.output.payload.statusCode);
             return newResponse;
         }
- 
- 
+
         return h.continue;
     });
- 
+
     await server.start();
-    console.log(`Server start at: ${server.info.uri}`);
+    console.log(`Server started at: ${server.info.uri}`);
 })();
